43fc4e0af559cc77412e04b63550d77ea20ae588
SuGii <sugii.noob@gmail.com>
2023-12-14 08:34:16 +0100

    Navbar updated

--- Thorvald-blog-front-end/src/Components/Navbar/Navbar.jsx
+++ Thorvald-blog-front-end/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
 import './Navbar.scss'
+import drakkarLogo from './img/drakkar-logo.png'
+import instaLogo from './img/insta-Logo.png'
+
 
 export default function Navbar() {
-    return(
+    return (
         <>
-            <h1>salut</h1>
+            <div className='navbar-container'>
+                <div className='navbar-left-part'>
+                    <a href="#"><img src={drakkarLogo} alt="drakkar-logo" /></a>
+                </div>
+                <div className='navbar-middle-part'>
+                    <ul>
+                        <li><a href="#">Voyages</a></li>
+                        <li><a href="#">Voyages</a></li>
+                        <li><a href="#">Voyages</a></li>
+                        <li><a href="#">Voyages</a></li>
+                        <li><a href="#">Voyages</a></li>
+                        
+                    </ul>
+                </div>
+                <div className='navbar-right-part'>
+                    <a href="#"><img src={instaLogo} alt="drakkar-logo" /></a>
+                </div>
+            </div>
+
         </>
-    )   
+    )
 }
