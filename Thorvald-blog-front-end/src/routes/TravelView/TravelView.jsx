8d4561a2bc8b295676fb7d836b6d8eeac7e8abb2
SuGii <sugii.noob@gmail.com>
2024-01-12 08:40:57 +0100

    travelView css updated

--- Thorvald-blog-front-end/src/routes/TravelView/TravelView.jsx
+++ Thorvald-blog-front-end/src/routes/TravelView/TravelView.jsx
@@ -1,20 +1,19 @@
 import "./TravelView.scss"
-
-import {useLoaderData,} from "react-router-dom";
+import { useLoaderData, } from "react-router-dom";
 
 export default function TravelView() {
-
-    
 
     const travel = useLoaderData();
     console.log(travel[0].banner)
     return (
-        <div className="travelView-container">  
+        <div className="travelView-container">
             <div className="travelView-banner">
                 <img src={travel[0].banner} alt="travel-landscape" />
             </div>
-            <h2>bienvenue sur {travel[0].planet}</h2>
-            <p>{travel[0].title}</p>
-        </div>    
+            <div className="travelView-content">
+                <h1>bienvenue sur {travel[0].planet}</h1>
+                <h2>{travel[0].subTitle}</h2>
+            </div>
+        </div>
     )
 }
