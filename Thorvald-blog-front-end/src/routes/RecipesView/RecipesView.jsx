8b99127b5718767408a0f9c2437268bf440bc069
SuGii <sugii.noob@gmail.com>
2024-02-07 23:21:51 +0100

    RecipesCards updated - RecipesView updated / responsiv - RecipesData updated

--- Thorvald-blog-front-end/src/routes/RecipesView/RecipesView.jsx
+++ Thorvald-blog-front-end/src/routes/RecipesView/RecipesView.jsx
@@ -8,11 +8,18 @@ export default function RecipesView() {
             <div className="recipeList-banner" style={{ backgroundImage: `url(https://placehold.co/1920x400)` }}>
             </div>
             <h1>Cuisine D'ailleurs</h1>
+            <p>Découvrez un univers culinaire extraordinaire ! Parcourez les recettes exotiques 
+                et savoureuses dénichées lors de mes voyages intergalactiques à travers les étoiles. 
+                Des mets inspirés des cultures et des saveurs des planètes lointaines vous attendent, 
+                prêts à vous transporter vers de nouvelles dimensions gustatives. Laissez-vous séduire 
+                par la diversité gastronomique de l'univers et embarquez pour un voyage gustatif inoubliable.</p>
+            <div className="recipe-galery-container">
             {recipesData.map((recipe, index) => {
                 return(
-                    <RecipeCard key={index} recipeName={recipe.name} type={recipe.type} imgThumb={recipe.imgThumbnail}/>
+                    <RecipeCard className="recipe-card" key={index} recipeName={recipe.name} type={recipe.type} imgThumb={recipe.imgThumbnail}/>
                 )
-            })}           
+            })} 
+            </div>                    
         </div>
     )
 }
