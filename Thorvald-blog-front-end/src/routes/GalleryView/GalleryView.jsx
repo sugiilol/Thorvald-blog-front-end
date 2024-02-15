a172f2f576dd6adf897a98c8485d4e7e8385e644
SuGii <sugii.noob@gmail.com>
2024-02-15 08:08:50 +0100

    GalleryView created - galleryImgs.json created

--- Thorvald-blog-front-end/src/routes/GalleryView/GalleryView.jsx
+++ Thorvald-blog-front-end/src/routes/GalleryView/GalleryView.jsx
@@ -1,12 +1,40 @@
 import './galleryView.scss'
 
-export default function GalleryView(){
-    return(
+export default function GalleryView() {
+    return (
         <div className='gallery-container'>
-            <h1>Galerie photo</h1>
-            <div className='gallery-content'>
-
-            </div>
+            <h1>Galerie</h1>
+            <ul className='grid-gallery-content'>
+                <li><img src="https://placehold.co/500x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/450x300" alt="alt" /></li>
+                <li><img src="https://placehold.co/300x500" alt="alt" /></li>
+                <li><img src="https://placehold.co/300x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/400x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/300x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/500x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/400x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/500x300" alt="alt" /></li>
+                <li><img src="https://placehold.co/400x500" alt="alt" /></li>
+                <li><img src="https://placehold.co/300x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/400x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/600x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/500x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/200x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/500x300" alt="alt" /></li>
+                <li><img src="https://placehold.co/500x500" alt="alt" /></li>
+                <li><img src="https://placehold.co/300x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/400x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/400x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/500x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/600x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/500x300" alt="alt" /></li>
+                <li><img src="https://placehold.co/300x500" alt="alt" /></li>
+                <li><img src="https://placehold.co/300x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/400x200" alt="alt" /></li>
+                <li><img src="https://placehold.co/400x400" alt="alt" /></li>
+                <li><img src="https://placehold.co/500x400" alt="alt" /></li>
+            </ul>
         </div>
+
     )
 }
