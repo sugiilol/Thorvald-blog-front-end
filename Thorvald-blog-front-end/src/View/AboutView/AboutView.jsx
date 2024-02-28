c1886f295624ef7d2ad4aabdbb30a80a612a702a
SuGii <sugii.noob@gmail.com>
2024-02-28 07:49:00 +0100

    ancor added for rocketButton to AboutView

--- Thorvald-blog-front-end/src/View/AboutView/AboutView.jsx
+++ Thorvald-blog-front-end/src/View/AboutView/AboutView.jsx
@@ -1,19 +1,19 @@
 import './AboutView.scss'
 import thorvaldAvatar from "./img/Thorvald-avatar.png"
 
-export default function AboutView(){
-    return(
-        <div className='about-container'>
+export default function AboutView() {
+    return (
+        <div className='about-container' id='aboutView'>
             <h2>"Thorvald Astraforge : Explorateur Viking de l'Espace, Narrateur des Étoiles"</h2>
             <div className='about-content'>
                 <div className='about-content__left-part'>
-                Dans les confins infinis de l'univers, je suis Thorvald Astraforge, un viking intrépide qui a échangé les fjords gelés 
-                pour les étoiles scintillantes.<br />
-                Embarquez avec moi à bord de "L'Éclat Céleste" et plongez dans les mystères de l'espace, 
-                où chaque étoile raconte une histoire, chaque planète révèle un secret, et chaque galaxie offre une aventure nouvelle.<br />
-                Bienvenue dans le récit interstellaire de mon exploration, où la magie des étoiles rencontre la force viking dans un ballet 
-                cosmique sans fin.<br />
-                Que la lumière des étoiles guide notre chemin !
+                    Dans les confins infinis de l'univers, je suis Thorvald Astraforge, un viking intrépide qui a échangé les fjords gelés
+                    pour les étoiles scintillantes.<br />
+                    Embarquez avec moi à bord de "L'Éclat Céleste" et plongez dans les mystères de l'espace,
+                    où chaque étoile raconte une histoire, chaque planète révèle un secret, et chaque galaxie offre une aventure nouvelle.<br />
+                    Bienvenue dans le récit interstellaire de mon exploration, où la magie des étoiles rencontre la force viking dans un ballet
+                    cosmique sans fin.<br />
+                    Que la lumière des étoiles guide notre chemin !
                 </div>
                 <div className='about-content__right-part'>
                     <img src={thorvaldAvatar} alt="Thorvald-avatar" />
--- Thorvald-blog-front-end/src/View/FirstHomeBgView/FirstHomeBgView.jsx
+++ Thorvald-blog-front-end/src/View/FirstHomeBgView/FirstHomeBgView.jsx
@@ -4,9 +4,10 @@ export default function FirstHomeBgView() {
     return (
         
             <div className='FirstHomeBgView-container'>             
-                    <button className='rocket-button'>
+                    {/* <button className='rocket-button'>
                         <span className="material-symbols-outlined">rocket_launch</span>
-                    </button>               
+                    </button>       */}
+                    <a href="#aboutView" className='rocket-button'><span className="material-symbols-outlined">rocket_launch</span></a>         
             </div>     
     )
 }
